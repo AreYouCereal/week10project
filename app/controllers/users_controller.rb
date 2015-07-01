00cbf5a5c62be1da7426e9a048f671739a5d22e2
Wendi Speciale <Wendi@CreateSalesAndMarketing.com>
2015-07-01 10:30:24 -0500

    adding controller for updated user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,20 +31,26 @@ end
 
 def create
   @user = User.new(user_params)
-
-  respond_to do |format|
-    if @user.save
-
-      # Sends email to user when user is created.
-      SendEmailJob.set(wait: 20.seconds).perform_later(@user)
-      # Below might need to be commented if error
-      ExampleMailer.sample_email(@user).deliver
-
-      format.html { redirect_to @user, notice: 'User was successfully created.' }
-      format.json { render :show, status: :created, location: @user }
-    else
-      format.html { render :new }
-      format.json { render json: @user.errors, status: :unprocessable_entity }
-    end
+  if user.save
+    redirect_to @user
+  else
+    render 'new'
   end
 end
+
+
+#     if @user.save
+#
+#       # Sends email to user when user is created.
+#       SendEmailJob.set(wait: 20.seconds).perform_later(@user)
+#       # Below might need to be commented if error
+#       ExampleMailer.sample_email(@user).deliver
+#
+#       format.html { redirect_to @user, notice: 'User was successfully created.' }
+#       format.json { render :show, status: :created, location: @user }
+#     else
+#       format.html { render :new }
+#       format.json { render json: @user.errors, status: :unprocessable_entity }
+#     end
+#   end
+# end
